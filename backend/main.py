bbe7bf94a29e8d549e62f66e9986da059697d13a
Haider <belzeebubc@gmail.com>
2021-12-27 08:59:16 -0500

    making flask route

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jasonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 # intialize the flask app
@@ -9,3 +9,14 @@ app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resouces={r'/*': {'origins': '*'}})
+
+# route to the index page
+
+
+@app.route('/', methods=['GET'])
+def greeting():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
